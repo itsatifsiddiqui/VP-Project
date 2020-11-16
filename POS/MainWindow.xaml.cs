5db54478823d79ce6de7168e261c4e4b45f60bfe
Atif Siddiqui <itsatifsiddiqui@gmail.com>
2020-11-16 12:06:34 -0800

    login screen and dashboard navigation rail setup

---
+++ POS/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onLoginButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            string usernamee = "itsatifsiddiqui@gmail.com";
+            string pssword = "123456";
+
+            if (emailField.Text != "" && passwordField.Password != "")
+            {
+                if (emailField.Text == usernamee && passwordField.Password == pssword)
+                {
+                    
+                    home home = new home();
+                    home.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to login Try again\nIncorrect username or password", "Failed");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in the email and password.","Empty");
+            }
+
+        }
+    }
+}
